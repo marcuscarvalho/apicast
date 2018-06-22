36e0ea1d2f9e6a9bf073ee1cdefad9b034254d20
Michal Cichra <michal@cichra.cz>
2018-06-22 15:37:24 +0200

    [resty.concurrent] safe task executor
    http://ruby-concurrency.github.io/concurrent-ruby/Concurrent/SafeTaskExecutor.html

---
+++ spec/resty/concurrent/safe_task_executor_spec.lua
@@ -0,0 +1,24 @@
+local SafeTaskExecutor = require('resty.concurrent.safe_task_executor')
+
+describe('SafeTaskExecutor', function()
+    it('has module level execute', function()
+        local task = spy.new(function () end)
+
+        SafeTaskExecutor.execute(task)
+
+        assert.spy(task).was_called(1)
+    end)
+
+    it('returns success', function()
+        local task = SafeTaskExecutor.new(function () return 'some value', 'some other value' end)
+
+        assert.same({true, { 'some value', 'some other value' }}, { task:execute() })
+    end)
+
+    it('returns failure', function ()
+        local task = SafeTaskExecutor.new(function () return error('some error') end)
+
+        assert.same({false, nil, 'some error'}, { task:execute() })
+    end)
+
+end)
